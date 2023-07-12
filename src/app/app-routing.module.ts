2adf2c8e3231f8eaf6ee493aa64ad4d87672fbb2
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2023-07-12 10:06:33 -0300

    Criacao de Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {CriarPensamentoComponent} from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
+import {ListarPensamentosComponent} from "./componentes/pensamentos/listar-pensamentos/listar-pensamentos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component'
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import {FormsModule} from "@angular/forms";
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CriarPensamentoComponent
+    CriarPensamentoComponent,
+    ListarPensamentosComponent
   ],
   imports: [
     BrowserModule,
